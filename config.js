e891fc98bf81cf339ba7f50720408ad962b5334f
nsarka00@gmail.com <nsarka00@gmail.com>
2017-07-05 17:16:50 -0400

    First commit

---
+++ config.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+    steamApiKey: '5724FDE13EAACD6BB0CD6238E984A341',    // Your Steam API key, get it here: https://steamcommunity.com/dev/apikey
+    SteamApisKey: '2eGArj4s4kTojLTBMaoJk0q3I5c',   // Your SteamApis.com key, get it here: https://steamapis.com
+
+    site: {
+        header: 'csoptic.com', // Name/header/title of website. Prefix for  <title></title> (For more: /index.html line: 9) 
+        steamGroup: '#',
+        copyrights: 'Copyright © CsOptic.com 2017',  // Copyright text
+    },
+    domain: 'localhost',    // Domain name only, follow the example (no http:// & no www & no /)
+    website: 'http://localhost',    // Website URL, follow the example (do not add / at the end)
+    websitePort: 80,    // Website PORT, don't change it unless you're using a reverse proxy
+    tradeMessage: 'Trade offer from CsOptic | If you did not request this offer or the offer looks invalid please decline.', // Quite obvious
+}
---
+++ server.js
@@ -0,0 +1,105 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const passport = require('passport');
+const session = require('express-session');
+const sharedsession = require('express-socket.io-session');
+const SteamStrategy = require('passport-steam').Strategy;
+
+const config = require('./config');
+const TradeBot = require('./lib/index')
+
+const Trade = new TradeBot({ io })
+
+passport.serializeUser(function(user, done) {
+  done(null, user)
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj)
+});
+
+passport.use(new SteamStrategy({
+    returnURL: config.website + '/auth/steam/return',
+    realm: config.website,
+    apiKey: config.steamApiKey
+  },
+  function(identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Steam profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Steam account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile)
+    });
+  }
+));
+
+const sessionMiddleware = session({
+    secret: 'csg0tradebot',
+    name: config.domain,
+    resave: true,
+    saveUninitialized: true,
+})
+
+app.use(sessionMiddleware)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(express.static(__dirname + '/public'))
+
+// Auth Routes
+app.get('/auth/steam', passport.authenticate('steam'))
+app.get('/auth/steam/return', passport.authenticate('steam', { failureRedirect: '/auth/steam' }), (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/')
+})
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.use(sharedsession(sessionMiddleware))
+io.on('connection', function(socket){
+  let userObject = false
+    if (
+      typeof socket.handshake.session.passport !== 'undefined' &&
+      typeof socket.handshake.session.passport.user !== 'undefined' &&
+      typeof socket.handshake.session.passport.user.id !== 'undefined'
+    ) {
+      userObject = socket.handshake.session.passport.user
+    }
+
+  socket.emit('site', config.site)
+  socket.emit('user', userObject)
+
+  socket.on('get user inv', (steamID64) => {
+    Trade.getInventory(steamID64, '730', '2', (err, data) => {
+      socket.emit('user inv', { error: err, items: data })
+    })
+  })
+
+  socket.on('get pricelist', () => {
+    socket.emit('pricelist', Trade.getPriceList())
+  })
+
+  socket.on('chat message', function(msg){
+    if(msg.name) {
+      io.emit('chat message', msg);
+    }
+  });
+});
+
+http.listen(config.websitePort, function(){
+  console.log('listening on *:' + config.websitePort);
+});
