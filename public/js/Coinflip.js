d40d1c0aa994439562c3a77958fca962266632b6
Digglit <Josephmarella97@gmail.com>
2017-08-15 00:12:41 -0500

    Actually visable coinflip
    *splooge*

--- public/js/Coinflip.js
+++ public/js/Coinflip.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 
-var spinArray = ['animation900','animation1080','animation1260','animation1440','animation1620','animation1800','animation1980','animation2160'];
+var spinArray = ['animation900','animation1080','animation1260','animation1440'];
 
 function getSpin() {
 var spin = spinArray[Math.floor(Math.random()*spinArray.length)];
