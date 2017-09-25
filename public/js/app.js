2bdd429fdedc93097d94af93656903c01681dae1
Digglit <Josephmarella97@gmail.com>
2017-09-24 22:48:28 -0500

    watch modal dismissal

--- public/js/app.js
+++ public/js/app.js
@@ -143,6 +143,7 @@ $(function () {
             },
             flipCoin: function(index) {
                 const self = this
+				var ModalOpened = Coinflips[flipIndexClicked];
                 console.log('flipCoin called')
                 if(typeof self.coinflips[index].ctWin !== 'null' && typeof self.coinflips[index].ctWin !== 'undefined') {
                     console.log('ctWin exists')
