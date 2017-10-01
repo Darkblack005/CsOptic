d2c658d6bdcc7da547e14b1bbb7cd1f246a1f29f
Digglit <Josephmarella97@gmail.com>
2017-10-01 14:21:37 -0500

    aids

--- public/js/app.js
+++ public/js/app.js
@@ -143,7 +143,7 @@ $(function () {
             },
             flipCoin: function(index) {
                 const self = this
-				var ModalOpened = self.Coinflips[this.flipIndexClicked];
+				var ModalOpened = self.coinflips[this.flipIndexClicked];
                 console.log('flipCoin called')
                 if(typeof self.coinflips[index].ctWin !== 'null' && typeof self.coinflips[index].ctWin !== 'undefined') {
                     console.log('ctWin exists')
